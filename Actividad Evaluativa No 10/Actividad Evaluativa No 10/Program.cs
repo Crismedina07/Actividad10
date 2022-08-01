6f3763c9d3a294bd4c7fa057ead6caf3e05515ef
Crismedina07 <41929688+Crismedina07@users.noreply.github.com>
2022-08-01 16:25:21 -0400

    adición de manejo de errores y rutina para volver al inicio

--- Actividad Evaluativa No 10/Actividad Evaluativa No 10/Program.cs
+++ Actividad Evaluativa No 10/Actividad Evaluativa No 10/Program.cs
@@ -10,61 +10,88 @@ namespace Actividad_Evaluativa_No_10
     {
         static void Main(string[] args)
         {
-            double salarioAnual = 0;
-            double salarioMensual = 0;
-            double retencion = 0;
-            int caso = 0;
+            //Voler al inicio en caso que el usuario desee realizar varias operaciones
+            inicio:
+            try
+            {
+                double salarioAnual = 0;
+                double salarioMensual = 0;
+                double retencion = 0;
+                int caso = 0;
+                double rentaAnual = 0;
+                string desicion;
 
 
-            Console.Write("Ingrese su salario mensual: ");
-            salarioMensual = double.Parse(Console.ReadLine());
-            salarioAnual = salarioMensual * 12;
-            double rentaAnual = 0;
-
-            if (salarioAnual <= 416200.00)
-            {
-                caso = 1;
-            }
-            else if (salarioAnual >= 416200.01 && salarioAnual <= 624329.00)
-            {
-                caso = 2;
-            }
-            else if (salarioAnual >= 624329.01 && salarioAnual <= 867123.00)
-            {
-                caso = 3;
-            }
-            else if (salarioAnual >= 867123.01)
-            {
-                caso = 4;
-            }
-            else
-            {
-                caso = 0;
-            }
-
-            if (caso !=0)
-            {
-                switch (caso)
+                Console.Write("Ingrese su salario mensual: ");
+                salarioMensual = double.Parse(Console.ReadLine());
+                salarioAnual = salarioMensual * 12;
+                
+                //Evaluar el caso segun el salario anual
+                if (salarioAnual <= 416200.00)
                 {
-                    case 1:
-                        rentaAnual = 0;
-                        break;
-                    case 2:
-                        rentaAnual = (salarioAnual - 416220.01) * 0.15;
-                        break;
-                    case 3:
-                        rentaAnual = (salarioAnual - 624329.01) * 0.20 + 31216.00;
-                        break;
-                    case 4:
-                        rentaAnual = (salarioAnual - 867123.01) * 0.25 + 79776.00;
-                        break;
+                    caso = 1;
                 }
-                retencion = rentaAnual / 12;
-                Console.Write("La retención Mensual será de: " + retencion);
-                Console.ReadKey();
+                else if (salarioAnual >= 416200.01 && salarioAnual <= 624329.00)
+                {
+                    caso = 2;
+                }
+                else if (salarioAnual >= 624329.01 && salarioAnual <= 867123.00)
+                {
+                    caso = 3;
+                }
+                else if (salarioAnual >= 867123.01)
+                {
+                    caso = 4;
+                }
+                else
+                {
+                    caso = 0;
+                }
+
+                //evaluar que el caso no sea 0
+                if (caso != 0)
+                {
+                    //Dependiendo del caso realizar la operacion correspondiente
+                    switch (caso)
+                    {
+                        case 1:
+                            rentaAnual = 0;
+                            break;
+                        case 2:
+                            rentaAnual = (salarioAnual - 416220.01) * 0.15;
+                            break;
+                        case 3:
+                            rentaAnual = (salarioAnual - 624329.01) * 0.20 + 31216.00;
+                            break;
+                        case 4:
+                            rentaAnual = (salarioAnual - 867123.01) * 0.25 + 79776.00;
+                            break;
+                    }
+                    retencion = rentaAnual / 12;
+                    Console.WriteLine("La retención Mensual será de: " + retencion);
+                    Console.WriteLine("Desea procesar otro salario? (s = Si n = No");
+                    desicion = Console.ReadLine();
+                    //Voler al inicio en caso que el usuario desee realizar varias operaciones
+                    if (desicion == "S")
+                    {
+                        goto inicio;
+                    }
+                    else
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+
+
             }
+            //Manejo de errores
+            catch (Exception ex)
+            {
 
-
+                Console.WriteLine("Ha ocurrido un error inesperado: " + ex + " Verifique si el valor insertado es valido");
+                //Voler al inicio para que el usuario ingrese el valor adecuado
+                goto inicio;
+            }
         }
     }
 }
